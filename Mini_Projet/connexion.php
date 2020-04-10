4fc58f83f050458f8fdca5209d1a62a2ed2f8028
Babacar SISSOKO <sissokobabacar@gmail.com>
2020-04-10 20:08:53 +0200

    Inrégration page connexion

---
+++ Mini_Projet/connexion.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+    
+    <head>
+        <title>Connexion</title>
+        <link href="MinProj.css" rel="stylesheet">
+    </head>
+    
+    <body>
+        
+        <div class="div1">
+            
+            <header>
+                <img src="Images/logo-QuizzSA.png">
+                <h1>Le plaisir de jouer</h1>
+            </header>
+            
+            <div class="div2">
+                <div class="div3">
+                    <div class="div4">
+                        <h2>Login Form</h2>
+                    </div>
+                    
+                    <div class="div5">  
+                        <div class="div6">
+                            <input type="text" placeholder="Login">
+                            <img src="Images/Icônes/ic-login.png">
+                        </div>
+ 
+                        <div class="div6">
+                            <input type="text" placeholder="Password">
+                            <img src="Images/Icônes/ic-password.png">
+                        </div>
+                        
+                        <div class="div7">
+                            <input class="Connexion" type="submit" value="Connexion">
+                            <a href="#"><h3>S'inscrire pour jouer?</h3></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            
+            
+        </div>
+    
+    </body>
+
+
+</html>
